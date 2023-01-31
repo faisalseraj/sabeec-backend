5c8a8a5204bb100bc6c04abf534b74985cd4a264
Faisal Seraj <59417212+faisalseraj@users.noreply.github.com>
2023-01-31 11:58:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-
+app.use("/", (req, res) => {res.send("server is up and running")})
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/site', siteRouter);
 app.use('/api/v1/reader', readerRouter);
